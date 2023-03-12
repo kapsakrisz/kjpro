43980dd965910a3d93655f62f0d75d05f400c7e6
krisztian kaposi <krisztiankaposi@hotmail.com>
2023-03-12 15:41:28 +0000

    size c3

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -14,13 +14,12 @@ function Projects({}:Props) {
         transition={{duration:1.5}}
         
         
-        className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row
-        max-w-full justify-evenly mx-auto items-center z-0">
+        className="flex flex-col relative h-screen text-center md:text-left
+        md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
             <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">References</h3>
 
-          <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
-            
-              <ProjectCard1/>  
+          <div className="">
+            <ProjectCard1/>  
                
 
                  
