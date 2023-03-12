d4d74f720ea494fc6dd51093a52a9a5cdd187f65
krisztian kaposi <krisztiankaposi@hotmail.com>
2023-03-12 15:29:06 +0000

    sizechange1

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -20,8 +20,8 @@ function ContactMe({}:Props) {
 
 
     return (
-        <div className="h-screen flex relative flex-col text-center md:text-left 
-        md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+        <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row
+        max-w-full justify-evenly mx-auto items-center z-0">
             <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
                 contact
             </h3>
